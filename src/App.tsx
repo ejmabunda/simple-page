3e3fdf95408c87c6ad05ade5a2504ae8f85359e1
Junior <mjmabunda0@gmail.com>
2024-02-28 15:56:16 +0200

    First commit

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import './App.css'
+
+function App() {
+  return (
+    <>
+      <section className='coming-soon'>
+        <h1>Coming Soon!</h1>
+        <p>Building something cool here, stay tuned!</p>
+      </section>
+    </>
+  )
+}
+
+export default App
